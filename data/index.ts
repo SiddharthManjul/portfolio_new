f4635772ac9dbe99200f1a0e84e97021edffaddc
SiddharthManjul <manjulsiddharth09@gmail.com>
2024-10-31 23:36:09 +0530

    V1 Done

---
+++ data/index.ts
@@ -0,0 +1,227 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Achievements", link: "#achievements" },
+  { name: "Approach", link: "#approach" },
+  { name: "Contact", link: "#contact" },
+
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize collaborations and teamwork, fostering long-term relations.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2 min-h-60",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex flex-col justify-center",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "A Tech savvy enthusiast, with a passion for Decentralized Internet & AI Infrastructure.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building NimbusArc, a Decentralized Storage.",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "NimbusArc",
+    des: "NimbusArc is a Decentralized Storage built using Hadoop Principles and integrated with Interchain Stack",
+    img: "/NimbusArc.png",
+    iconLists: [],
+    link: "https://dorahacks.io/buidl/18174",
+  },
+  {
+    id: 2,
+    title: "aoVest",
+    des: "aoVest is a Vesting Platform built using AO and part of Arweave Ecosystem.",
+    img: "/aovest.png",
+    iconLists: [],
+    link: "https://aovest.stream/",
+  },
+  {
+    id: 3,
+    title: "DevEye AI",
+    des: "DevEye AI is an AI based application which generate Codes and Docs using the available context & aims to provide solutions to user queries.",
+    img: "/DevEyeAI.png",
+    iconLists: [],
+    link: "https://dev-eye-ai.vercel.app",
+  },
+  {
+    id: 4,
+    title: "BookingBox-TX",
+    des: "Built a Decentralized Hotel/Homestay booking Dapp leveraging powers of Blockchain Technology.",
+    img: "/bookingbox.png",
+    iconLists: [],
+    link: "https://booking-box-tx.vercel.app/",
+  },
+]
+
+export const achievements = [
+  {
+    id: 1,
+    quote:
+      "I executed the research part for aoVest and built the architecture and coded backend during Hacker House.",
+    img: "arweaveWin.jpeg",
+    title: "Winner, Arweave Hacker House Cohort #1",
+  },
+  {
+    id: 2,
+    quote:
+      "Came second in this National level 72 hour hackathon. We developed a suite to allow specially abled people to use a computer with less effort.",
+    img: "hackvengers.png",
+    title: "2nd Runner-Ups Hackvengers.",
+  },
+  {
+    id: 3,
+    quote:
+      "I was appointed as the Web Development Lead for Google Developer Student Club - UCER for the year 2022-2023.",
+    img: "GDSC.jpeg",
+    title: "GDSC Web Development Lead",
+  },
+  // {
+  //   id:4,
+  //   quote:
+  //     "Was selected as the contributo",
+  //     img: "gssoc.png",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+  // {
+  //   id:5,
+  //   quote:
+  //     "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+  //   img: "./nothing.jpeg",
+  //   title: "Director of AlphaStream Technologies",
+  // },
+];
+
+// export const companies = [
+//   {
+//     id: 1,
+//     name: "cloudinary",
+//     img: "/cloud.svg",
+//     nameImg: "/cloudName.svg",
+//   },
+//   {
+//     id: 2,
+//     name: "appwrite",
+//     img: "/app.svg",
+//     nameImg: "/appName.svg",
+//   },
+//   {
+//     id: 3,
+//     name: "HOSTINGER",
+//     img: "/host.svg",
+//     nameImg: "/hostName.svg",
+//   },
+//   {
+//     id: 4,
+//     name: "stream",
+//     img: "/s.svg",
+//     nameImg: "/streamName.svg",
+//   },
+//   {
+//     id: 5,
+//     name: "docker.",
+//     img: "/dock.svg",
+//     nameImg: "/dockerName.svg",
+//   },
+// ];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "FHE.org",
+    desc: "Contributing to FHE.org in further development and improvement of Fully Homomorphic Encryption (FHE) as a community member.",
+    className: "md:col-span-2",
+    thumbnail: "fhe.png",
+  },
+  {
+    id: 2,
+    title: "Cubane",
+    desc: "Developed Cubane Cubic Consensus Mechanism (CCCM) Architecture as a Blockchain Researcher & Developer",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/cubane.jpeg",
+  },
+  {
+    id: 3,
+    title: "Web Dev Lead",
+    desc: "Led the Web track for GDSC UCER, conducted cohort and taught over 150 students with Practical Hands-On and projects.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "GDSC.jpeg",
+  },
+  {
+    id: 4,
+    title: "More Incoming",
+    desc: "Actively looking to gather more Experiece, drop me an email if you think that I can help you.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    link: "https://github.com/SiddharthManjul",
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    link: "https://x.com/ManjulSiddharth",
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    link: "https://www.linkedin.com/in/siddharth-manjul/",
+    img: "/link.svg",
+  },
+];
